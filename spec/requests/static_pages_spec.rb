cebcea79102b29c033a3294a528be65f6495dbe5
Mike Meloro <michael.meloro08@gmail.com>
2012-05-07 12:17:45 -0400

    Added Users and Signup Links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,8 +22,8 @@ describe "Static pages" do
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About Us')) }
+    it { should have_selector('h1',    text: 'About Us') }
+    it { should have_selector('title', text: full_title('About')) }
   end
 
   describe "Contact page" do
